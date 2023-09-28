5eacf0d8f9766b639ed5c5c08782bf4559d4b91d
Nicolas Haase <nh@lutzhaase.com>
2023-09-28 11:12:45 +0200

    Dialog utility now an instantiable class

--- presentation/util/dialog-utility.mjs
+++ presentation/util/dialog-utility.mjs
@@ -1,47 +1,55 @@
 /**
- * Shows a confirmation dialog. 
- * @param {Object} args Optional arguments to pass to the rendering function. 
- * @param {String} args.localizableTitle Localization string for the dialog title. 
- * @param {String | undefined} args.content Optional. HTML content to show as the body of the dialog. 
- * @returns {Promise<Boolean>} Resolves, when the dialog is closed. 
- *          Is true, when the dialog was closed with confirmation. 
- * @async
+ * Provides convenience functions for showing dialogs to the user. 
  */
-export async function showConfirmationDialog(args = {}) {
-  args = {
-    localizableTitle: "",
-    ...args
-  };
-  const mergedDialogData = {
-    confirmed: false
-  };
-
-  return new Promise(async (resolve, reject) => {
-    const dialog = new Dialog({
-      title: game.i18n.localize(args.localizableTitle),
-      content: args.content ?? "",
-      buttons: {
-        confirm: {
-          icon: '<i class="fas fa-check"></i>',
-          label: game.i18n.localize("wg.general.confirm"),
-          callback: () => {
-            mergedDialogData.confirmed = true;
+export default class DialogUtility {
+  /**
+   * Shows a confirmation dialog. 
+   * 
+   * @param {Object} args Optional arguments to pass to the rendering function. 
+   * @param {String} args.localizableTitle Localization string for the dialog title. 
+   * @param {String | undefined} args.content Optional. HTML content to show as the body of the dialog. 
+   * 
+   * @returns {Promise<Boolean>} Resolves, when the dialog is closed. 
+   * * Is `true`, when the dialog was closed with confirmation. 
+   * 
+   * @async
+   */
+  async showConfirmationDialog(args = {}) {
+    args = {
+      localizableTitle: "",
+      ...args
+    };
+    const mergedDialogData = {
+      confirmed: false
+    };
+  
+    return new Promise(async (resolve, reject) => {
+      const dialog = new Dialog({
+        title: game.i18n.localize(args.localizableTitle),
+        content: args.content ?? "",
+        buttons: {
+          confirm: {
+            icon: '<i class="fas fa-check"></i>',
+            label: game.i18n.localize("wg.general.confirm"),
+            callback: () => {
+              mergedDialogData.confirmed = true;
+            }
+          },
+          cancel: {
+            icon: '<i class="fas fa-times"></i>',
+            label: game.i18n.localize("wg.general.cancel"),
+            callback: () => { }
           }
         },
-        cancel: {
-          icon: '<i class="fas fa-times"></i>',
-          label: game.i18n.localize("wg.general.cancel"),
-          callback: () => { }
+        default: "cancel",
+        render: html => { },
+        close: html => {
+          resolve({
+            confirmed: mergedDialogData.confirmed,
+          });
         }
-      },
-      default: "cancel",
-      render: html => { },
-      close: html => {
-        resolve({
-          confirmed: mergedDialogData.confirmed,
-        });
-      }
+      });
+      dialog.render(true);
     });
-    dialog.render(true);
-  });
+  }
 }
