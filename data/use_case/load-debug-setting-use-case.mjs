9dfb5810c079a4862da03fa2e4e96da5fc832106
Nicolas Haase <nh@lutzhaase.com>
2023-09-28 21:13:24 +0200

    Deleted the now obsolete use cases

--- data/use_case/load-debug-setting-use-case.mjs
+++
@@ -1,9 +0,0 @@
-import CustomUserSettings from "../settings/custom-user-settings.mjs";
-import AbstractUseCase from "./abstract-use-case.mjs";
-
-export default class LoadDebugSettingUseCase extends AbstractUseCase {
-  invoke(args) {
-    const ds = new CustomUserSettings();
-    return ds.get(CustomUserSettings.KEY_TOGGLE_DEBUG);
-  }
-}
