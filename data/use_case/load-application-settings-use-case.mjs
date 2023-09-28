186baa87f823241cf53c8928b8ca876aa9f3bace
Nicolas Haase <nh@lutzhaase.com>
2023-09-28 11:59:10 +0200

    Remove `WordGeneratorApplicationDataSource` and its use cases
    * This data source is obsolete, as it is replaced by `WordGeneratorApplicationSettingsDataSource`. The data contained not so important as to warrant migration, however. This is why it is removed without deprecation period.

--- data/use_case/load-application-settings-use-case.mjs
+++
@@ -1,9 +0,0 @@
-import WordGeneratorApplicationDataSource from "../presentation/data/word-generator-application-datasource.mjs";
-import AbstractUseCase from "./abstract-use-case.mjs";
-
-export default class LoadApplicationSettingsUseCase extends AbstractUseCase {
-  invoke(userId) {
-    const ds = new WordGeneratorApplicationDataSource();
-    return ds.get(userId);
-  }
-}
