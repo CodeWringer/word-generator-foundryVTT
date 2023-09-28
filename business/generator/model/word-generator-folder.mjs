ca2f01198dc943ec6e1a84b65f98c9fb1d2452f7
Nicolas Haase <nh@lutzhaase.com>
2023-09-28 11:18:41 +0200

    Docs improvements

--- business/generator/model/word-generator-folder.mjs
+++ business/generator/model/word-generator-folder.mjs
@@ -6,9 +6,45 @@
  * @property {Array<String>} itemIds IDs of the contained word generators or other folders. 
  */
 export default class WordGeneratorFolder {
+  /**
+   * @param {Object} args 
+   * @param {String | undefined} id Unique ID of the folder. 
+   * @param {String | undefined} name Human readable name of the folder. 
+   * @param {Array<String> | undefined} itemIds IDs of the contained word generators or other folders. 
+   */
   constructor(args = {}) {
     this.id = args.id ?? foundry.utils.randomID(16);
     this.name = args.name ?? "New Folder";
     this.itemIds = args.itemIds ?? [];
+  }
+  
+  /**
+   * Returns an instance of this type parsed from the given data transfer object. 
+   * 
+   * @param {Object} obj 
+   * 
+   * @returns {WordGeneratorFolder}
+   * 
+   * @static
+   */
+  static fromDto(obj) {
+    return new WordGeneratorFolder({
+      id: obj.id,
+      name: obj.name,
+      itemIds: obj.itemIds,
+    });
+  }
+
+  /**
+   * Returns a data transfer object representation of this instance. 
+   * 
+   * @returns {Object}
+   */
+  toDto() {
+    return {
+      id: this.id,
+      name: this.name,
+      itemIds: this.itemIds,
+    };
   }
 }
--- business/generator/model/word-generator-item.mjs
+++ business/generator/model/word-generator-item.mjs
@@ -10,25 +10,30 @@ import CharDepthSequencingStrategy from "../sequencing/char-depth-sequencing-str
  * @property {String | undefined} name Human readable name. 
  * @property {Array<String>} sampleSet A sample-set to draw inspiration from. 
  * @property {String} sampleSetSeparator The string used to split samples. This splitting happens **prior** to sequencing. 
- * @property {Number} depth 
+ * @property {Number} depth The depth to use with the sequencing strategy. 
  * 
- * @property {Number} targetLengthMin
- * @property {Number} targetLengthMax
+ * @property {Number} targetLengthMin The target minimum length that generated texts should be. 
+ * @property {Number} targetLengthMax The target maximum length that generated texts should be. 
  * 
- * @property {String} sequencingStrategyId
- * @property {Array<StrategySetting>} sequencingStrategySettings
+ * @property {String} sequencingStrategyId ID of the sequencing strategy to use. 
+ * @property {Array<StrategySetting>} sequencingStrategySettings Settings of the sequencing strategy. 
  * 
- * @property {String | undefined} spellingStrategyId
- * @property {Array<StrategySetting> | undefined} spellingStrategySettings
+ * @property {String | undefined} spellingStrategyId ID of the spelling post-processing strategy to use. 
+ * @property {Array<StrategySetting> | undefined} spellingStrategySettings Settings of the spelling post-processing strategy. 
  * 
- * @property {String} seed
+ * @property {String} seed A seed for the randomization. 
  * 
- * @property {Number} entropy
- * @property {Number} entropyStart
- * @property {Number} entropyMiddle
- * @property {Number} entropyEnd
+ * @property {Number | undefined} entropy A number between 0 and 1 (inclusive), which determines the 
+ * randomness of words, in general. Default 0. 
+ * @property {Number | undefined} entropyStart A number between 0 and 1 (inclusive), which determines the 
+ * randomness of starting sequences. Default 0. 
+ * @property {Number | undefined} entropyMiddle A number between 0 and 1 (inclusive), which determines the 
+ * randomness of middle sequences. Default 0. 
+ * @property {Number | undefined} entropyEnd A number between 0 and 1 (inclusive), which determines the 
+ * randomness of ending sequences. Default 0. 
  * 
- * @property {ENDING_PICK_MODES} endingPickMode
+ * @property {ENDING_PICK_MODES} endingPickMode Determines how and if an ending sequence 
+ * will be picked for generated words. 
  * 
  * @property {Boolean} collapsed If true, the entry is to be rendered collapsed. 
  */
