02abd3ad28800f0f8ba10b72858d719f68ab2475
Nicolas Haase <nh@lutzhaase.com>
2023-09-28 11:55:58 +0200

    Removed mock use case
    * There is no point in delivering mock data to production code.

--- data/settings/custom-user-settings.mjs
+++ data/settings/custom-user-settings.mjs
@@ -11,13 +11,6 @@ export default class CustomUserSettings extends CustomSettings {
    * @type {String}
    * @readonly
    */
-  static get KEY_TOGGLE_MOCK() { return "toggleMock"; }
-
-  /**
-   * @static
-   * @type {String}
-   * @readonly
-   */
   static get KEY_TOGGLE_DEBUG() { return "toggleDebug"; }
 
   constructor()
@@ -25,15 +18,6 @@ export default class CustomUserSettings extends CustomSettings {
     super();
 
     this._settings.push(
-      new CustomSetting({
-        key: CustomUserSettings.KEY_TOGGLE_MOCK,
-        name: game.i18n.localize("wg.settings.toggleMock.name"),
-        hint: undefined,
-        scope: SettingScopes.USER,
-        config: true,
-        default: false,
-        type: Boolean,
-      }),
       new CustomSetting({
         key: CustomUserSettings.KEY_TOGGLE_DEBUG,
         name: game.i18n.localize("wg.settings.toggleDebug.name"),
--- data/use_case/load-mock-setting-use-case.mjs
+++
@@ -1,9 +0,0 @@
-import CustomUserSettings from "../settings/custom-user-settings.mjs";
-import AbstractUseCase from "./abstract-use-case.mjs";
-
-export default class LoadMockSettingUseCase extends AbstractUseCase {
-  invoke(args) {
-    const ds = new CustomUserSettings();
-    return ds.get(CustomUserSettings.KEY_TOGGLE_MOCK);
-  }
-}
