256dc9b2d990b9e402622bab67f8a2a2873bdac8
Nicolas Haase <nh@lutzhaase.com>
2023-10-02 19:22:58 +0200

    `TypeRegistrar` now only handles objects of type `AbstractStrategyDefinition`

--- business/model/type-registrar/type-registrar.mjs
+++ business/model/type-registrar/type-registrar.mjs
@@ -1,19 +1,15 @@
+import AbstractStrategyDefinition from "../../generator/common/abstract-strategy-definition.mjs";
+
 /**
- * @summary
- * This abstract type defines the basic contract with which to 'register' certain types of classes, 
- * so that they may be fetched by ID. 
+ * Defines the basic contract with which to 'register' strategy definitions. 
+ * Only accepts objects of type `AbstractStrategyDefinition`. 
  * 
- * @description
- * Prevents registering the same type twice and **mandates** that objects to register implement these methods: 
- * * `getDefinitionID()` - must return a String. 
- * * `newInstanceWithArgs(args)` - must return a new instance of that type of definition. 
- * 
- * @abstract
+ * Prevents registering the same type twice. 
  */
 export default class TypeRegistrar {
   /**
    * The list of registered types. 
-   * @type {Array<Object>}
+   * @type {Array<AbstractStrategyDefinition>}
    * @private
    */
   _registered = [];
@@ -21,20 +17,15 @@ export default class TypeRegistrar {
   /**
    * Registers a given type definition, if it isn't already registered. 
    * 
-   * @param {Object} type The 'definition' to register. 
+   * @param {AbstractStrategyDefinition} type The 'definition' to register. 
    * 
-   * @throws {Error} Thrown, in case the given type lacks a 'getDefinitionID' method definition. 
    * @throws {Error} Thrown, in case the given type is already registered. 
    */
   register(type) {
-    if (type.getDefinitionID === undefined) {
-      throw new Error("The given object lacks a 'getDefinitionID' method definition");
-    }
-
-    const existing = this._registered.find(it => it.getDefinitionID() === type.getDefinitionID())
+    const existing = this._registered.find(it => it.id === type.id)
     
     if (existing !== undefined) {
-      throw new Error(`The given type with id ${type.getDefinitionID()} is already registered`);
+      throw new Error(`The given type with id ${type.id} is already registered`);
     } else {
       this._registered.push(type);
     }
@@ -45,18 +36,12 @@ export default class TypeRegistrar {
    * 
    * Returns true, if the type definition could be removed. 
    * 
-   * @param {Object} type The 'definition' to un-register. 
+   * @param {AbstractStrategyDefinition} type The 'definition' to un-register. 
    * 
    * @returns {Boolean} True, if the type definition could be removed. 
-   * 
-   * @throws {Error} Thrown, in case the given type lacks a 'getDefinitionID' method definition. 
    */
   unregister(type) {
-    if (type.getDefinitionID === undefined) {
-      throw new Error("The given object lacks a 'getDefinitionID' method definition");
-    }
-
-    const index = this._registered.findIndex(it => it.getDefinitionID() === type.getDefinitionID());
+    const index = this._registered.findIndex(it => it.id === type.id);
 
     if (index >= 0) {
       this._registered.splice(index, 1);
@@ -64,20 +49,21 @@ export default class TypeRegistrar {
   }
 
   /**
-   * Returns a registered type identified by the given ID or `undefined`, in case no type with the given ID is registered. 
+   * Returns a registered type identified by the given ID or `undefined`, 
+   * in case no type with the given ID is registered. 
    * 
    * @param {String} id ID of a type definition to return. 
    * 
-   * @returns {Object | undefined} The definiton object or `undefined`.
+   * @returns {AbstractStrategyDefinition | undefined} The definiton object or `undefined`.
    */
   get(id) {
-    return this._registered.find(it => it.getDefinitionID() === id);
+    return this._registered.find(it => it.id === id);
   }
 
   /**
    * Returns all registered type definitions. 
    * 
-   * @returns {Array<Object>} All registered type definitions. 
+   * @returns {Array<AbstractStrategyDefinition>} All registered type definitions. 
    */
   getAll() {
     return this._registered.concat([]); // Return a safe copy. 
@@ -87,21 +73,18 @@ export default class TypeRegistrar {
    * Returns a new instance of the type identified by the given ID. 
    * 
    * @param {String} id ID of a type definition to return. 
-   * @param {Object} constructorArgs A parameter-object to pass to the `newInstanceWithArgs`-method of the definition. 
+   * @param {Object} constructorArgs A parameter-object to pass to the `newInstance`-
+   * method of the definition. 
    * 
    * @returns {Object} A new instance of the given registered type. 
    */
   newInstanceOf(id, constructorArgs) {
-    const existing = this._registered.find(it => it.getDefinitionID() === id)
+    const existing = this._registered.find(it => it.id === id)
 
     if (existing === undefined) {
       throw new Error(`No type with id ${id} registered`);
     }
 
-    if (existing.newInstanceWithArgs === undefined) {
-      throw new Error("The object lacks a 'newInstanceWithArgs' method definition");
-    }
-    
-    return existing.newInstanceWithArgs(constructorArgs);
+    return existing.newInstance(constructorArgs);
   }
 }
